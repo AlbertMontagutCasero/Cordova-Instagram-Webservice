4c63376db3d51d08c4ea3602d44099e4ec797bf2
Raikish <albert.montagut.casero@gmail.com>
2018-05-05 13:04:26 +0200

    generating random name file to prevent duplicated files.

--- UploadImage.php
+++ UploadImage.php
@@ -46,17 +46,21 @@ if ($_FILES["file"]["error"] > 0)
     return;
 }
 
-if (file_exists("uploads/" . $_FILES["file"]["name"]))
-{
-    echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
-} else
-{
-    $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
-    $targetPath = "uploads/" . $_FILES['file']['name']; // Target path where file is to be stored
-    move_uploaded_file($sourcePath, $targetPath); // Moving Uploaded file
-    echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
-    echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
-    echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
-    echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+$fileName = generateRandomString(30); // Target path where file is to be stored
+move_uploaded_file($_FILES['file']['tmp_name'],"uploads/$fileName.jpg");
+
+echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
